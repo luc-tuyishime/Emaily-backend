afd38d819142d329a457a0f92b834fee5b6435a3
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2019-09-19 20:23:50 +0200

    new change

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 // app.js
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import indexRouter from './routes/index';
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/', indexRouter);
+
+app.get('/', (req, res, next) => {
+  res.send({ message: 'Welcome to the Emaily Feedback...' });
+});
+
 export default app;
