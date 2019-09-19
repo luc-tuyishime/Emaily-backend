1853af123521a771d4ba8f363ca438cddbf64153
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2019-09-19 18:55:43 +0200

    add nodemon to development

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send({ hi: there });
 });
 
 export default router;
